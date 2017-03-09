830524518b22de17549a342d67539854b9de8daa
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-03-09 10:54:57 -0800

    Initial setup

---
+++ 30-Days-of-Code/Day11/2D-arrays.js
@@ -0,0 +1,50 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var arr = [];
+    for(arr_i = 0; arr_i < 6; arr_i++){
+       arr[arr_i] = readLine().split(' ');
+       arr[arr_i] = arr[arr_i].map(Number);
+    }
+    var sum = [];
+
+    var h = 0;
+    for (var i = 0; i < 4; i++) { //Loop through only 4 elements for the first row
+        for (var j = 0; j < 4; j++) {
+            sum[h] = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                    + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
+                    + arr[i+2][j+2];
+            h++;
+        }
+    }
+
+    sum.sort();
+
+    var highest = -99999;
+    for (var x = 0; x < 16; x++){
+        if (sum[x] > highest)
+            highest = sum[x];
+    }
+
+    console.log(highest);
+}
