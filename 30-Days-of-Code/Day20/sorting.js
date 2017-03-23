b89aa8184997ed5e0260071d6bb1c109b122ad11
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-03-23 14:22:33 -0700

    Initial commit

---
+++ 30-Days-of-Code/Day20/sorting.js
@@ -0,0 +1,43 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    a = readLine().split(' ');
+    a = a.map(Number);
+
+    var totalSwap = 0;
+    var temp;
+    for(var i = a.length - 1; i >= 0; i--) {
+        for(var j = 1; j <= i; j++) {
+            if (a[j - 1] > a[j]) {
+                temp = a[j - 1];
+                a[j - 1] = a[j];
+                a[j] = temp;
+                totalSwap++;
+            }
+        }
+    }
+    console.log("Array is sorted in " + totalSwap + " swaps.");
+    console.log("First Element: " + a[0]);
+    console.log("Last Element: " + a[a.length - 1]);
+}
